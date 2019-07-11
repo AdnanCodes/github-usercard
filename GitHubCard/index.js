e680cca6e1d6ac94805efe79e0c02c6de4fb8151
Adnan <adnan.chowdhury@outlook.com>
2019-07-11 17:38:19 -0500

    Added others users to be passed into API pull and renders on the HTML

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,8 +4,13 @@
 */
 
 const cards = document.querySelector('.cards')
+//const user = 'AdnanWebDev'
 
-axios.get(`https://api.github.com/users/AdnanWebDev`)
+
+const followersArray = ['tetondan','dustinmyers', 'justsml', 'luishrd', 'bigknell','AdnanWebDev'];
+
+followersArray.forEach(user => {
+axios.get(`https://api.github.com/users/${user}`)
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -24,7 +29,7 @@ axios.get(`https://api.github.com/users/AdnanWebDev`)
 .catch(error => {
   console.log('API is currently down', error)
 })
-
+})
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -34,8 +39,6 @@ axios.get(`https://api.github.com/users/AdnanWebDev`)
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
-
-const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
